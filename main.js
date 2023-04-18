132fdb884ff59118e83c8654ffd5176ce7eba79b
Giorgiopri22 <giorgioprizzi@gmail.com>
2023-04-18 15:00:15 +0200

    Fine es

---
+++ main.js
@@ -0,0 +1,14 @@
+const { createApp } = Vue 
+
+
+createApp({
+   data(){
+    return {
+        message: "Hello World!",
+        image: "https://picsum.photos/200/300"
+    }
+   },
+   methods:{
+
+   } 
+}).mount("#app")
